77aaf0efce84c8ba6768810d3f3c79c07685d808
putta <putta@DESKTOP-KIHN7FQ>
2021-05-13 00:32:27 +0530

    Initial Commit

---
+++ OnlineSportsShopee/src/test/java/com/example/OnlineSportsShopee/OnlineSportsShopeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OnlineSportsShopee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineSportsShopeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
