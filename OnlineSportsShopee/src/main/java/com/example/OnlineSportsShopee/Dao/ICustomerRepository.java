090749633d923c3f773c3bcb84b8e257b10bb4e2
HP <HP@LAPTOP-QQDE9910>
2021-05-15 10:40:27 +0530

    CustomerRepo

--- OnlineSportsShopee/src/main/java/com/example/OnlineSportsShopee/Dao/ICustomerRepository.java
+++ OnlineSportsShopee/src/main/java/com/example/OnlineSportsShopee/Dao/ICustomerRepository.java
@@ -1,5 +1,10 @@
 package com.example.OnlineSportsShopee.Dao;
 
-public interface ICustomerRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.OnlineSportsShopee.Entities.CustomerEntity;
 
+@Repository
+public interface ICustomerRepository extends JpaRepository<CustomerEntity, String> {
+	
 }
