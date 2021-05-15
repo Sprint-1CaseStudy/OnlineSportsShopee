607642d6a6eddfdc66111e0ad6b4fc2aecbac6bc
putta <putta@DESKTOP-KIHN7FQ>
2021-05-15 10:59:43 +0530

    Product Repository

--- OnlineSportsShopee/src/main/java/com/example/OnlineSportsShopee/Services/ProductService.java
+++ OnlineSportsShopee/src/main/java/com/example/OnlineSportsShopee/Services/ProductService.java
@@ -1,5 +1,15 @@
 package com.example.OnlineSportsShopee.Services;
 
+import java.util.List;
+
+import com.example.OnlineSportsShopee.Model.Product;
+
+
 public interface ProductService {
+	
+	public List<Product> getProductsByName();
+	public List<Product> getProductsBySize(); 
+	public List<Product> getProductsByPrice();
+	public List<Product> getProductsByColor();
 
 }
--- OnlineSportsShopee/src/main/java/com/example/OnlineSportsShopee/Services/ProductServiceImpl.java
+++ OnlineSportsShopee/src/main/java/com/example/OnlineSportsShopee/Services/ProductServiceImpl.java
@@ -1,6 +1,37 @@
 package com.example.OnlineSportsShopee.Services;
 
-public class ProductServiceImpl {
-	//azeem
-///lll
+import java.util.List;
+
+import com.example.OnlineSportsShopee.Model.Product;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+	
+	@Override
+	public List<Product> getProductsByName()
+	{
+		return list;
+		
+	}
+	
+	@Override
+	public List<Product> getProductsBySize()
+	{
+		
+	}
+	
+	@Override
+	public List<Product> getProductsByPrice()
+	{
+		
+	}
+	
+	@Override
+	public List<Product> getProductsByColor()
+	{
+		
+	}
+	
 }
