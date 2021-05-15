afac246913ff9f9334810939349c1bc384eb9ae1
dell <dell@DESKTOP-SAL80FM>
2021-05-15 19:19:20 +0530

    order commit12

--- OnlineSportsShopee/src/main/java/com/example/onlinesportshopee/services/IOrderService.java
+++ OnlineSportsShopee/src/main/java/com/example/onlinesportshopee/services/IOrderService.java
@@ -1,5 +1,14 @@
 package com.example.onlinesportshopee.services;
 
+import com.example.onlinesportshopee.entities.OrderEntity;
+import com.example.onlinesportshopee.model.Order;
+import java.util.List;
+
 public interface IOrderService {
+public Order addOrder(OrderEntity orderEntity);
+public Order updateOrder(long id,OrderEntity orderEntity);
+public Order deleteOrder(long id);
+public Order getOrderDetails(long id);
+public List<Order> getAllOrders();
 
 }
--- OnlineSportsShopee/src/main/java/com/example/onlinesportshopee/services/OrderServiceImpl.java
+++ OnlineSportsShopee/src/main/java/com/example/onlinesportshopee/services/OrderServiceImpl.java
@@ -1,8 +1,28 @@
 package com.example.onlinesportshopee.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.onlinesportshopee.dao.IOrderRepository;
+import com.example.onlinesportshopee.entities.OrderEntity;
+import com.example.onlinesportshopee.model.Order;
+import com.example.onlinesportshopee.util.OrderUtils;
 
 @Service
 public class OrderServiceImpl {
 
+	@Autowired 
+	private IOrderRepository ordrepo; 
+	
+	@Override
+	public Order addOrder(OrderEntity orderEntity) {
+		OrderEntity ordEntity;
+		if(orderEntity==null)
+			ordEntity=null;
+		else {
+			ordEntity=ordrepo.save(orderEntity);
+			
+		}
+		return OrderUtils.(ordEntity);
+	}
 }
