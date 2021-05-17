06c53e9e549eefce04d746e4c58674db979dc8c7
putta <putta@DESKTOP-KIHN7FQ>
2021-05-17 10:35:54 +0530

    product controller

--- OnlineSportsShopee/src/main/java/com/example/onlinesportshopee/controller/ProductController.java
+++ OnlineSportsShopee/src/main/java/com/example/onlinesportshopee/controller/ProductController.java
@@ -1,14 +1,47 @@
 package com.example.onlinesportshopee.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.web.bind.annotation.*;
 
 import com.example.onlinesportshopee.dao.IProductRepository;
+import com.example.onlinesportshopee.model.Product;
+
 
 @RestController
 public class ProductController {
 	
 	@Autowired
 	private IProductRepository productRepository;
+	
+	
+	@GetMapping("/products/{name}")
+	public List<Product> getProductsByName(@PathVariable String name)
+	{
+		return null;
+		
+	}
+	
+	@GetMapping("/products/{size}")
+	public List<Product> getProductsBySize(@PathVariable String size)
+	{
+		return null;
+		
+	}
+	
+	@GetMapping("/products/{price}")
+	public List<Product> getProductsByPrice(@PathVariable double price)
+	{
+		return null;
+		
+	}
+	
+	@GetMapping("/products/{color}")
+	public List<Product> getProductsByColor(@PathVariable String color)
+	{
+		return null;
+		
+	}
 
 }
