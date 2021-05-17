62112a2fb8065887afd7e560e9d89daf5a2193eb
putta <putta@DESKTOP-KIHN7FQ>
2021-05-17 10:14:18 +0530

    product changes

--- OnlineSportsShopee/src/main/java/com/example/onlinesportshopee/controller/ProductController.java
+++ OnlineSportsShopee/src/main/java/com/example/onlinesportshopee/controller/ProductController.java
@@ -1,5 +1,14 @@
 package com.example.onlinesportshopee.controller;
 
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.onlinesportshopee.dao.IProductRepository;
+
+@RestController
 public class ProductController {
+	
+	@Autowired
+	private IProductRepository productRepository;
 
 }
