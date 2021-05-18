8cbb28001af28be11a247c16f95508dbb032e209
putta <putta@DESKTOP-KIHN7FQ>
2021-05-18 15:18:59 +0530

    user utils

--- OnlineSportsShopee/src/main/java/com/example/onlinesportshopee/services/IUserService.java
+++ OnlineSportsShopee/src/main/java/com/example/onlinesportshopee/services/IUserService.java
@@ -1,12 +1,13 @@
 package com.example.onlinesportshopee.services;
 
 
+import com.example.onlinesportshopee.Exception.UserException;
 import com.example.onlinesportshopee.entities.UserEntity;
 import com.example.onlinesportshopee.model.User;
 
 public interface IUserService {
 
-	public User signIn(UserEntity user);
+	public User signIn(UserEntity user) throws UserException;
 	public User signOut(UserEntity user);
 	public User changePassword(long id,UserEntity user);
 
--- OnlineSportsShopee/src/main/java/com/example/onlinesportshopee/services/UserServiceImpl.java
+++ OnlineSportsShopee/src/main/java/com/example/onlinesportshopee/services/UserServiceImpl.java
@@ -1,22 +1,44 @@
 package com.example.onlinesportshopee.services;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.onlinesportshopee.Exception.UserException;
 import com.example.onlinesportshopee.dao.IUserRepository;
 import com.example.onlinesportshopee.entities.UserEntity;
 import com.example.onlinesportshopee.model.User;
+import com.example.onlinesportshopee.util.UserUtils;
 
 @Service
 public class UserServiceImpl implements IUserService {
 	
 	@Autowired
-	private IUserRepository UserServiceImpl;
+	private IUserRepository Userrepo;
 
 	@Override
-	public User signIn(UserEntity user) {
-		// TODO Auto-generated method stub
-		return null;
+	public User signIn(UserEntity user) throws UserException {
+		String userid = user.getUserid();
+		String password = user.getPassword();
+		UserEntity useridrepo = Userrepo.findById(userid).orElse(null);
+		if (useridrepo == null)
+		{
+			String usernotfound = "No user found by the userid "+userid;
+			throw new UserException(usernotfound);
+		}
+		else 
+		{
+			if(userid.equals(useridrepo.getUserid()) && password.equals(useridrepo.getPassword()))
+			{
+				return null;
+			}
+			else
+			{
+				return null;
+			}
+		}
 	}
 
 	@Override
@@ -27,8 +49,15 @@ public class UserServiceImpl implements IUserService {
 
 	@Override
 	public User changePassword(long id, UserEntity user) {
-		// TODO Auto-generated method stub
-		return null;
+		UserEntity userEnti;
+		UserEntity changePassword = Userrepo.findById(id).orElse(null);
+		if(changePassword == null)
+		{
+			String usernotfound = "No user found by the userid "+userid;
+			throw new UserException(usernotfound);
+		}
+		else userEnti = Userrepo.save(user);
+		return UserUtils.convertToOrder(userEnti);
 	}
 
 }
