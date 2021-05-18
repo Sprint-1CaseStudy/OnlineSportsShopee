6fee8c2dd7cd85ac3f9ac96779f4beb259192931
Raja <REHMAN@DESKTOP-611RJMF>
2021-05-18 10:07:57 +0530

    paymentServiceImp

--- OnlineSportsShopee/src/main/java/com/example/onlinesportshopee/services/PaymentServiceImpl.java
+++ OnlineSportsShopee/src/main/java/com/example/onlinesportshopee/services/PaymentServiceImpl.java
@@ -5,44 +5,46 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.cg.cars.util.PaymentUtils;
 import com.example.onlinesportshopee.dao.IPaymentRepository;
 import com.example.onlinesportshopee.entities.PaymentEntity;
+import com.example.onlinesportshopee.entities.ProductEntity;
 import com.example.onlinesportshopee.model.Payment;
+import com.example.onlinesportshopee.util.*;
 
 @Service
 public class PaymentServiceImpl implements IPaymentService{
 
 	@Autowired
-	private IPaymentRepository PaymentServiceImpl;
+	private IPaymentRepository paymentRepository;
 	
 	@Override
 	public Payment addPayment(PaymentEntity payment) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Payment paymentEntity=payment.save(payment);
+		return PaymentUtils.convertToPayment(paymentEntity);
 	}
 
 	@Override
-	public Payment removePayment(long id) {
-		// TODO Auto-generated method stub
-		return null;
+	public Payment removePayment(long paymentID) {
+		PaymentServiceImpl.delete(PaymentUtils.convertToPayment(paymentID));
 	}
 
 	@Override
-	public Payment updatePayment(long id, PaymentEntity payment) {
-		// TODO Auto-generated method stub
-		return null;
+	public Payment updatePayment(long paymentID, PaymentEntity paymentEntity) {
+		paymentRepository.save(PaymentUtils.convertToPayment(paymentEntity));
 	}
 
 	@Override
-	public Payment getPaymentDetails(long id) {
-		// TODO Auto-generated method stub
-		return null;
+	public Payment getPaymentDetails(long paymentID) {
+		PaymentEntity getPaymentDetails= paymentRepository.findById(paymentId).get();
+		return PaymentUtils.convertToPayment(getPayment);
 	}
 
 	@Override
 	public List<Payment> getAllPaymentDetails() {
-		// TODO Auto-generated method stub
-		return null;
+		List<PaymentEntity> getAllPaymentDetails = paymentRepository.findAll();
+		return ProductUtils.convertToProductDtoList(getAllPaymentDetails);
 	}
 
 }
