3ed6f95260f36be5fcc9c637716cac0118494595
HP <HP@LAPTOP-QQDE9910>
2021-05-20 19:38:39 +0530

    ....

--- OnlineSportsShopee/src/main/java/com/example/onlinesportshopee/controller/CustomerController.java
+++ OnlineSportsShopee/src/main/java/com/example/onlinesportshopee/controller/CustomerController.java
@@ -2,6 +2,8 @@ package com.example.onlinesportshopee.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -18,31 +20,48 @@ import com.example.onlinesportshopee.services.ICustomerService;
 @RequestMapping("/customers")
 public class CustomerController 
 {
+	static final Logger LOGGER = LoggerFactory.getLogger(CustomerController.class);
+	
 	@Autowired
 	private ICustomerService iCustomerService;
 	
 	@PostMapping("/addCustomer")
 	public Customer addCustomer(@RequestBody CustomerEntity customer) throws CustomerNotFoundException {
+		LOGGER.info("addCustomer URL is opened");
+		LOGGER.info("addCustomer() is initiated");
+		LOGGER.info("addCustomer() is executed");
 		return iCustomerService.addCustomer(customer);
 	}
 	
 	@DeleteMapping("/removeCustomer/Customer/{custId}")
 	public Customer removeCustomer(@PathVariable long custId) throws CustomerNotFoundException {
+		LOGGER.info("removeCustomer URL is opened");
+		LOGGER.info("removeCustomer() is initiated");
+		LOGGER.info("removeCustomer() is executed");
 		return iCustomerService.removeCustomer(custId);
 	}
 
 	@PutMapping("/updateCustomer/{custId}")
 	public Customer updateCustomer(@PathVariable long custId,@RequestBody CustomerEntity customer) throws CustomerNotFoundException {
+		LOGGER.info("updateCustomer URL is opened");
+		LOGGER.info("updateCustomer() is initiated");
+		LOGGER.info("updateCustomer() is executed");
 		return iCustomerService.updateCustomer(custId, customer);
 	}
 
 	@GetMapping("/getCustomerDetails/{custId}")
 	public Customer getCustomer(@PathVariable long custId) throws CustomerNotFoundException {
+		LOGGER.info("getCustomerDetails URL is opened");
+		LOGGER.info("getCustomer() is initiated");
+		LOGGER.info("getCustomer() is executed");
 		return iCustomerService.getCustomer(custId);
 	}
 
 	@GetMapping("/Customers/{name}")
 	public List<Customer> getAllCustomers(@PathVariable String name) {
+		LOGGER.info("Customers URL is opened");
+		LOGGER.info("getAllCustomers() is initiated");
+		LOGGER.info("getAllCustomers() has executed");
 		return iCustomerService.getAllCustomers();
 	}
 	
