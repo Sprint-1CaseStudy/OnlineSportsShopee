c8ac6e1a80c60e6170a7be0b166ad00c193876fb
putta <putta@DESKTOP-KIHN7FQ>
2021-05-20 12:19:08 +0530

    Cart functionality

--- OnlineSportsShopee/src/main/java/com/example/onlinesportshopee/services/ICartService.java
+++ OnlineSportsShopee/src/main/java/com/example/onlinesportshopee/services/ICartService.java
@@ -1,5 +1,18 @@
 package com.example.onlinesportshopee.services;
 
-public class ICartService {
+import java.util.List;
+
+
+import com.example.onlinesportshopee.entities.CartEntity;
+import com.example.onlinesportshopee.exception.CartException;
+import com.example.onlinesportshopee.model.Cart;
+
+
+
+public interface ICartService {
+	
+		public Cart addCart(CartEntity cartEntity) throws CartException;
+		public Cart deleteCart(long id) throws CartException;
+		public List<Cart> getallCartDetails();
 
 }
