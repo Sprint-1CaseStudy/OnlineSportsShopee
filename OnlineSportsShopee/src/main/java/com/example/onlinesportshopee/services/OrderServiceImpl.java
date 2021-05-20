75bebeb12615b76c05382e24d79d1ab0397f02e5
dell <dell@DESKTOP-SAL80FM>
2021-05-20 19:57:51 +0530

    order loggers

--- OnlineSportsShopee/src/main/java/com/example/onlinesportshopee/services/OrderServiceImpl.java
+++ OnlineSportsShopee/src/main/java/com/example/onlinesportshopee/services/OrderServiceImpl.java
@@ -2,6 +2,8 @@ package com.example.onlinesportshopee.services;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,11 +17,13 @@ import com.example.onlinesportshopee.util.OrderUtils;
 @Service
 public class OrderServiceImpl implements IOrderService {
 
+	static final Logger LOGGER = LoggerFactory.getLogger(OrderServiceImpl.class);
 	@Autowired 
 	private IOrderRepository iOrderRepository; 
 	
 	@Override
 	public Order addOrder(OrderEntity orderEntity) throws OrderNotFoundException,InvalidOrderIdException{
+		LOGGER.info("addOrder() service is initiated");
 		OrderEntity ordEntity;
 		if(orderEntity==null)
 			ordEntity=null;
@@ -27,10 +31,12 @@ public class OrderServiceImpl implements IOrderService {
 			ordEntity=iOrderRepository.save(orderEntity);
 			
 		}
+		LOGGER.info("addOrder() service has executed");
 		return OrderUtils.convertToOrder(ordEntity);
 	}
 	@Override
 	public Order updateOrder(long id,OrderEntity orderEntity)throws OrderNotFoundException,InvalidOrderIdException{
+		LOGGER.info("updateOrder() service is initiated");
 		OrderEntity ordEntity;
 		OrderEntity existOrd= iOrderRepository.findById(id).orElse(null);
 		if (existOrd == null)
@@ -39,24 +45,27 @@ public class OrderServiceImpl implements IOrderService {
 			
 			ordEntity = iOrderRepository.save(orderEntity);
 		}
-		
+		LOGGER.info("updateOrder() service has executed");
 		return OrderUtils.convertToOrder(ordEntity);
 	}
 	@Override
 	public Order deleteOrder(long id) throws InvalidOrderIdException{
+		LOGGER.info("deleteOrder() service is initiated");
 		OrderEntity ordEntity = iOrderRepository.findById(id).orElse(null);
 		if (ordEntity == null)
 			throw new InvalidOrderIdException("orderNotFound");
 		else
 			iOrderRepository.delete(ordEntity);
+		LOGGER.info("deleteOrder() service has executed");
 		return OrderUtils.convertToOrder(ordEntity);
 	}
 	@Override
 	public Order getOrderDetails(long id)throws InvalidOrderIdException {
+		LOGGER.info("viewOrder() service is initiated");
 		OrderEntity ordEntity = iOrderRepository.findById(id).orElse(null);
 		if (ordEntity == null)
 			throw new InvalidOrderIdException("orderNotFound");
-		
+		LOGGER.info("viewOrder() service has executed");
 		return OrderUtils.convertToOrder(ordEntity);
 	}
 	@Override
